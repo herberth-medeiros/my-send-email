d6cfcb0290c9b5480f44b8fef5f601938ba6d666
seven-beto <herberthfreitas26@gmail.com>
2025-08-03 16:51:36 -0300

    Feat: Adicionando UserProducer mensageria rabbitMQ para criacao de usuario

--- ms-user/src/main/java/com/ms/user/service/UserService.java
+++ ms-user/src/main/java/com/ms/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.service;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -9,14 +10,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel){
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
-
 }
